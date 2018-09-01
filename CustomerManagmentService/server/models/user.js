24bbc86c13f29d7c68b74b693da7a31438683d7b
danial <daniel.kamali@yahoo.com>
2018-09-01 10:13:59 +0430

    customerModel

--- CustomerManagmentService/server/models/user.js
+++ CustomerManagmentService/server/models/user.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
+const CustomerSchema = new mongoose.Schema({
   username: {
     type: String, 
     required: true, 
@@ -11,30 +11,64 @@ const UserSchema = new mongoose.Schema({
     type: String, 
     required: true, 
     trim: true 
-  }
-});
+  },
+  fullname:{
+    type: String, 
+    required: true, 
+    trim: true
+  },
+  email:{
+    type: String, 
+    required: true, 
+    trim: true,
+  },
+  phonenumber:{
+    type: String, 
+    required: true, 
+    trim: true
+  },
+  active:{
+    type:boolean,
+    required: true, 
 
-UserSchema.pre('save', function (next) {
-    const user = this;
+  },
+  isDeleted:{
+    type:Boolean,
+    required:true,
+  },
+  point:{
+    type:Number,
+  }
+
+});
+/*
+This Method is crypting gotten password before saving
+*/
+CustomerSchema.pre('save', function (next) {
+    const customer = this;
   
-    if (!user.isModified('password')) {
+    if (!customer.isModified('password')) {
       return next();
     }
     bcrypt.genSalt(10, (err, salt) => {
       if (err) return next(err);
-      bcrypt.hash(user.password, salt, (hashErr, hash) => {
+      bcrypt.hash(customer.password, salt, (hashErr, hash) => {
         if (hashErr) return next(hashErr);  
-        user.password = hash;
+        customer.password = hash;
         next();
       });
     });
   });
 
-UserSchema.methods.comparePassword = function (toCompare, done) {
+/*
+This Method Comparing password in db with entered password
+*/
+CustomerSchema.methods.comparePassword = function (toCompare, done) {
     bcrypt.compare(toCompare, this.password, (err, isMatch) => {
       if (err) done(err);
       else done(err, isMatch);
     });
+
 };
 
-export default mongoose.model('User', UserSchema);
+export default mongoose.model('Customer', CustomerSchema);
