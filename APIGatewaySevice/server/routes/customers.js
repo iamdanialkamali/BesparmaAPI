a814d631e29405fa5e3d52ea52b62c8597db8bf7
hamedmiir <mirzaeihamed@yahoo.com>
2018-09-02 16:03:15 +0430

    routes for csutomer gateway

--- APIGatewaySevice/server/routes/customers.js
+++ APIGatewaySevice/server/routes/customers.js
@@ -3,10 +3,42 @@ import validate from 'express-validation'
 
 import customerCtrl from '../controllers/customers'
 import validations  from './validation/customers'
+import auth from '../../config/jwt';
 
 const router = express.Router()
 
 router.route('/register')
   .post(validate(validations.registerDataValidation), customerCtrl.register)
 
+
+router.route('/login')
+  /* Login customer */
+  .post(validate(validations.userPassValidation), customerCtrl.login)
+
+
+router.route('/update')
+  /* Update a customer */
+  .put(auth ,validate(validations.updateUserData), customerCtrl.update)
+
+router.route('/remove')
+  /* Remove a customer */
+  .delete(auth,customerCtrl.remove)
+
+router.route('/changePassword')
+  /* Change password */
+  .post(validate(validations.changePassword), customerCtrl.changePassword)
+
+router.route('/forgetPassword')
+  .post(validate(validations.forgetPassEmail), customerCtrl.forgetPassword)
+  
+router.route('/resetPassword')
+  .post(validate(validations.resetPassword), customerCtrl.resetPassword)
+
+
+router.route('/verify/:token')
+  .get(validate(validations.userPassValidation), customerCtrl.verify)
+
+router.route('/getMe')
+  .get(customerCtrl.getMe)
+  
 export default router
