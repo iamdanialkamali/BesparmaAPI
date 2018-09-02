9fe3455830f5416601692a1810afc963a47360be
hamedmiir <mirzaeihamed@yahoo.com>
2018-09-02 20:32:26 +0430

    routes for csutomer gateway

--- APIGatewaySevice/server/controllers/customers.js
+++ APIGatewaySevice/server/controllers/customers.js
@@ -1,12 +1,27 @@
 import client from '../../config/grpc'
 
 function register(req, res, next) {
-    client.register({},(err, data)=>{
-        username: req.body.username
+    client.register({
+        fullName : req.body.fullName,
+        email: req.body.email,
+        phoneNumber: req.body.phonenumber,
+        username: req.body.username,
         password: req.body.password
-        email: req.body.email
-        phoneNumber: req.body.phone
+    },(err, data)=>{
+        return res.json();
     })
 }
+
+
+function login(req, res, next) {
+    client.login({
+        username: req.body.username,
+        password: req.body.password
+    },(err, data)=>{
+        return res.json();
+    })
+}
+
+
 
 export default { register }
