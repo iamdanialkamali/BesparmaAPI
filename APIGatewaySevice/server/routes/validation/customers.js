c6b944becd4e11499bf3c29f878457847a83c9e9
a_otadi <ali.78.otadi@gmail.com>
2018-09-02 14:54:22 +0430

    validation complete ... Ready for create routes and get params

--- APIGatewaySevice/server/routes/validation/customers.js
+++ APIGatewaySevice/server/routes/validation/customers.js
@@ -7,7 +7,31 @@ export default {
       phonenumber: Joi.string().regex(/^[0-9]{11}$/).required(),
       username   : Joi.string().regex(/^[0-9a-zA-Z]{3,24}$/).required(),
       password   : Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).required(),
-      fullname   : Joi.string().regex(/^[0-9a-zA-Z\s]{6,24}$/).required(),
+      fullname   : Joi.string().regex(/^[0-9a-zA-Z\s]{6,24}$/).required()
+    }
+  },
+  userPassValidation: {
+    body: {
+        username: Joi.string().regex(/^[0-9a-zA-Z]{3,24}$/).required(),
+        password: Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).required()
+    }
+  },
+  forgetPassEmail: {
+    body: {
+      email: Joi.string().email({ minDomainAtoms: 2 }).required()
+    }
+  },
+  updateUserData: {
+    body: {
+      phonenumber: Joi.string().regex(/^[0-9]{11}$/),
+      username   : Joi.string().regex(/^[0-9a-zA-Z]{3,24}$/),
+      fullname   : Joi.string().regex(/^[0-9a-zA-Z\s]{6,24}$/)
+    }
+  },
+  changePassword: {
+    body: {
+      oldPassword: Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).required(),
+      newPassword: Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/).required()
     }
   }
 }
