9c323766ce9bd2c4365b145e07a66ce7f66d32be
hamedmiir <mirzaeihamed@yahoo.com>
2018-09-02 22:00:47 +0430

    routes for csutomer gateway

--- APIGatewaySevice/server/controllers/customers.js
+++ APIGatewaySevice/server/controllers/customers.js
@@ -2,17 +2,13 @@ import client from '../../config/grpc'
 
 function register(req, res, next) {
     const message = {
-        id   : req.customer.id,
-        status  : req.customer.status
-    }
-
-    client.register({
         fullName : req.body.fullName,
         email: req.body.email,
-        phoneNumber: req.body.phonenumber,
+        phoneNumber: req.body.phoneNumber,
         username: req.body.username,
         password: req.body.password
-    },(err, data)=>{
+    }
+    client.register(message , (error, data) => {
         if (error) res.sendStatus(500)
         res.status(data.code).send({ message: data.message })
     })
@@ -21,19 +17,14 @@ function register(req, res, next) {
 
 function login(req, res, next) {    
     const message = {
-        id   : req.customer.id
-    }
-
-    client.login({
         username: req.body.username,
         password: req.body.password
-    },(err, data)=>{
+    }
+    client.login(message,(error, data) =>{
         if (error) res.sendStatus(500)
         res.status(data.code).send({ message: data.message })
     })
 }
-
-
 
 
 function getMe(req, res, next) {
@@ -85,15 +76,14 @@ function forgetPassword(req, res, next) {
 
 function update(req, res, next) {
     const message = {
-      id   : req.customer.id
-    }
-
-    client.update({
-        fullName : req.body.fullName,
+        id: req.user.id,
+        fullName: req.body.fullName,
         email: req.body.email,
         phoneNumber: req.body.phonenumber,
         username: req.body.username
-    }),(message, (error, data) => {
+    }
+
+    client.update(message, (error, data) => {
       if (error) res.sendStatus(500)
       res.status(data.code).send({ message: data.message })
     })
@@ -102,25 +92,25 @@ function update(req, res, next) {
 
 function remove(req, res, next) {
     const message = {
-      id   : req.customer.id
+        id: req.user.id
     }
 
     client.remove(message, (error, data) => {
-      if (error) res.sendStatus(500)
-      res.status(data.code).send({ message: data.message })
+        if (error) res.sendStatus(500)
+        res.status(data.code).send({ message: data.message })
     })
 }
 
 
 function verify(req, res, next){
     const message = {
-        id   : req.customer.id
+        token: req.body.token
     }
 
     client.verify(message, (error, data) => {
         if (error) res.sendStatus(500)
         res.status(data.code).send({ message: data.message })
-      })
+    })
 }
 
 
