c48ae73e3d25724485d4ddcd1866e30707f20554
a_otadi <ali.78.otadi@gmail.com>
2018-09-02 17:36:30 +0430

    some controllers are ready

--- APIGatewaySevice/server/controllers/customers.js
+++ APIGatewaySevice/server/controllers/customers.js
@@ -9,4 +9,42 @@ function register(req, res, next) {
     })
 }
 
+
+function getMe(req, res, next) {
+  const message = { id: req.user.id }
+  
+  client.getMe(message, (error, data) => {
+      if (error) res.sendStatus(500)
+      res.status(data.code).send({ message: data.message })
+  })
+}
+
+function changePassword(req, res, next) {
+  const message = {
+    id          : req.user.id,
+    old_password: req.body.old_password,
+    new_password: req.body.new_password
+  }
+
+  client.changePassword(message, (error, data) => {
+    if (error) res.sendStatus(500)
+    res.status(data.code).send({ message: data.message })
+  })
+}
+
+function resetPassword(req, res, next) {
+    const message = {
+      token   : req.body.token,
+      password: req.body.password  
+    }
+
+    client.resetPassword(message, (error, data) => {
+      if (error) res.sendStatus(500)
+      res.status(data.code).send({ message: data.message })
+    })
+}
+
+function emailVerify(req, res, next){
+    
+}
 export default { register }
