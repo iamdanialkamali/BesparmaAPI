e4e0bf78ba347468c41630aeb3baf800dc82e2ba
hamedmiir <mirzaeihamed@yahoo.com>
2018-09-03 10:29:07 +0430

    routes for csutomer gateway

--- APIGatewaySevice/server/controllers/customers.js
+++ APIGatewaySevice/server/controllers/customers.js
@@ -27,39 +27,6 @@ function login(req, res, next) {
 }
 
 
-function getMe(req, res, next) {
-  const message = { id: req.user.id }
-  
-  client.getMe(message, (error, data) => {
-      if (error) res.sendStatus(500)
-      res.status(data.code).send({ message: data.message })
-  })
-}
-
-function changePassword(req, res, next) {
-  const message = {
-    id          : req.user.id,
-    old_password: req.body.old_password,
-    new_password: req.body.new_password
-  }
-
-  client.changePassword(message, (error, data) => {
-    if (error) res.sendStatus(500)
-    res.status(data.code).send({ message: data.message })
-  })
-}
-
-function resetPassword(req, res, next) {
-    const message = {
-      token   : req.body.token,
-      password: req.body.password  
-    }
-
-    client.resetPassword(message, (error, data) => {
-      if (error) res.sendStatus(500)
-      res.status(data.code).send({ message: data.message })
-    })
-}
 
 
 function forgetPassword(req, res, next) {
@@ -72,6 +39,7 @@ function forgetPassword(req, res, next) {
       res.status(data.code).send({ message: data.message })
     })
 }
+
 
 
 function update(req, res, next) {
@@ -114,4 +82,43 @@ function verify(req, res, next){
 }
 
 
-export default { register, login, getMe, update, remove, changePassword, forgetPassword, resetPassword, verify }
+
+function resetPassword(req, res, next) {
+    const message = {
+      token   : req.body.token,
+      password: req.body.password  
+    }
+
+    client.resetPassword(message, (error, data) => {
+      if (error) res.sendStatus(500)
+      res.status(data.code).send({ message: data.message })
+    })
+}
+
+
+function changePassword(req, res, next) {
+    const message = {
+      id          : req.user.id,
+      old_password: req.body.old_password,
+      new_password: req.body.new_password
+    }
+  
+    client.changePassword(message, (error, data) => {
+      if (error) res.sendStatus(500)
+      res.status(data.code).send({ message: data.message })
+    })
+  }
+
+
+
+function getMe(req, res, next) {
+    const message = { id: req.user.id }
+    
+    client.getMe(message, (error, data) => {
+        if (error) res.sendStatus(500)
+        res.status(data.code).send({ message: data.message })
+    })
+  }
+  
+
+export default { register, login, forgetPassword, update, remove, resetPassword, changePassword, getMe }
