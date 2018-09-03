7f27aa1e95a39277de0e8df7c70233add07592d0
danial <daniel.kamali@yahoo.com>
2018-09-03 15:12:57 +0430

    package.json

--- CustomerManagmentService/config/emailClient.js
+++ CustomerManagmentService/config/emailClient.js
@@ -1,12 +1,16 @@
 const grpc = require('grpc');
 
 
+const protoPath = require('path').join(__dirname, '', 'proto');
 
-const protoPath = require('path').join(__dirname, '../..', '/server/proto/');
+
 const proto = grpc.load({root: protoPath, file: 'email.proto' });
 
 //Create a new client instance that binds to the IP and port of the grpc server.
-const client = new proto.Email.emailManagementService('localhost:50051', grpc.credentials.createInsecure());
+const client = new proto.Email.emailManagementSevrice('localhost:50051', grpc.credentials.createInsecure());
 
 
+
+
+//Create a new client instance that binds to the IP and port of the grpc server.
 export default client;
