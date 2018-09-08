7ca7468c97d0dcf7fbd716f1f3965c6ceae1c117
hamedmiir <mirzaeihamed@yahoo.com>
2018-09-08 16:24:05 +0430

    fix test environment

--- APIGatewaySevice/config/env/test.js
+++ APIGatewaySevice/config/env/test.js
@@ -1,7 +1,8 @@
 export default {
     env: 'test',
-    db: 'mongodb://localhost/node-es6-api-test',
+    mongodb: 'mongodb://localhost/node-es6-api-test',
     port: 3000,
     jwtSecret: 'my-api-secret',
-    jwtDuration: '2 hours'
+    jwtDuration: '2 hours',
+    gRPCServer: 'localhost:50050'
   };
