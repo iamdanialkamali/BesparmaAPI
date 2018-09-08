16058b36ea5b6e00f3688d0a871c8465e0f363b8
a_otadi <ali.78.otadi@gmail.com>
2018-09-08 13:03:24 +0430

    create basic files and do imports for service provider gateway

---
+++ APIGatewaySevice/server/controllers/serviceProvider.js
@@ -0,0 +1,1 @@
+import client from '../../config/grpc'
---
+++ APIGatewaySevice/server/routes/serviceProvider.js
@@ -0,0 +1,5 @@
+import express  from 'express'
+import validate from 'express-validation'
+
+import customerCtrl from '../controllers/serviceProvider'
+import validations  from './validation/serviceProvider'
